1a3a30db199d81dfbd6adb654305f3e4665ff5c0
DESKTOP-65UV0V4\user <momen@qeematech.net>
2024-07-27 13:21:30 +0300

    - Delete Comment.

--- lib/afandem_app.dart
+++ lib/afandem_app.dart
@@ -5,7 +5,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 class AfandemApp extends StatelessWidget {
   const AfandemApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
